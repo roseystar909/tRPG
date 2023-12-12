e8c704f68ede6e6fff15c26f4257f732002d2c20
roseystar909 <witchesfromsalem@gmail.com>
2023-12-12 14:52:40 -0400

    collision fix

--- Game.c
+++ Game.c
@@ -49,81 +49,75 @@ void startScreen() {
   printl(85, 3, "Done!");
   delay(250);
 
-	// HEALTHBARS "Loading" screen
-	printl(72, 19, "[          ]");
+  // HEALTHBARS "Loading" screen
+  printl(72, 19, "[          ]");
 
-	for (int i = 73; i < 83; i++) {
-		printl(i, 19, "█");
-		delay(100);
-	}
-	printl(85, 19, "Done!");
-	delay(2500);
+  for (int i = 73; i < 83; i++) {
+  printl(i, 19, "█");
+  delay(100);
+  }
+  printl(85, 19, "Done!");
+  delay(2500);
 
-	// actual "Loading" screen
-	clearBigWindow();
-	clearSmallWindow();
-	clearSmallerWindow();
-	printl(2, 2, "Initiating GAME");
-	delay(500);
+  // actual "Loading" screen
+  clearBigWindow();
+  clearSmallWindow();
+  clearSmallerWindow();
+  printl(2, 2, "Initiating GAME");
+  delay(500);
 
-	printl(2, 3, "[          ]");
+  printl(2, 3, "[          ]");
 
-	for (int i = 3 ; i < 13; i++) {
-		printl(i, 3, "█");
-		delay(100);
-	}
-	printl(15, 3, "Done!");
-	delay(2500);
-	clearBigWindow();
+  for (int i = 3 ; i < 13; i++) {
+  printl(i, 3, "█");
+  delay(100);
+  }
+  printl(15, 3, "Done!");
+  delay(2500);
+  clearBigWindow();
 }
 
 void enemyMovement() {
-	printl(ENEMYx, ENEMYy, "X");
+  printl(ENEMYx, ENEMYy, "X");
 }
 
 void playerMovement() {
-	int x = 34;
-	int y = 24;
-	int prevX = x;
-	int prevY = y;
-	int key;
-	bool collided = false;
+  int x = 34;
+  int y = 24;
+  int key;
+  bool collided = false;
 
-	enemyMovement();
-	printl(x, y, "O");
+  enemyMovement();
+  printl(x, y, "O");
 
-	do {
-		key = getch();
+  do {
+    key = getch();
 
-		switch(key) {
-			case UP:
-				prevY = y;
-				--y;
-				if (y == MAPULIMIT) {
-					y++;
-				}
-				break;
-			case DOWN:
-				prevY = y;
-				++y;
-				if (y == MAPDLIMIT) {
+    switch(key) {
+      case UP:
+        --y;
+        if (y == MAPULIMIT) {
+          y++;
+        }
+        break;
+      case DOWN:
+        ++y;
+        if (y == MAPDLIMIT) {
           y--;
         }
         break;
       case LEFT:
-        prevX = x;
         --x;
         if (x == MAPLLIMIT) {
           x++;
         }
         break;
       case RIGHT:
-        prevX = x;
         ++x;
         if (x == MAPRLIMIT) {
           x--;
-        }
-        break;
+          }
+          break;
       case ESC:
         return;
         break;
@@ -134,16 +128,18 @@ void playerMovement() {
     delay(10);
     clearBigWindow();
 
-    if ((x == ENEMYx && y == ENEMYy) || (x == ENEMYx+1 || x == ENEMYx-1) || (y == ENEMYy+1 || y == ENEMYy-1)) {
+    if ((x == ENEMYx || x == ENEMYx + 1 || x == ENEMYx - 1) && (y == ENEMYy || y == ENEMYy + 1 || y == ENEMYy - 1)) {
       collided = true;
     } else {
       collided = false;
     }
 
     if (collided) {
-      printl(2, 2, "WHAT");
+      printl(2, 2, "collision: true");
+    } else {
+      printl(2, 2, "collision: false");
     }
-
+    
     enemyMovement();
     printl(x, y, "O");
   } while(1);
